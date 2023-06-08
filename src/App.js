0cbe0c47787af116d98c2377b86621d709257f4a
rzechary <102223789+rzechary@users.noreply.github.com>
2023-06-08 09:29:50 +0300

    08_06

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleAddSupply = () => {
+    if (counter < 5) {
+      setCounter(counter + 1);
+    } else {
+      setCounter(0);
+    }
+  };
+
+  const handleRemoveSupply = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container ">
+      <h2>Counter: {counter}</h2>
+      <button onClick={handleAddSupply} className="btn btn-dark">+</button>
+      <button onClick={handleRemoveSupply} className="btn btn-dark">-</button>
     </div>
   );
 }
